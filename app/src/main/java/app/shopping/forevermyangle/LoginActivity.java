b096b0db2ab2c90e018269769ea236eff6028c4c
rahulrainahomeee <rahulrainahome@gmail.com>
2017-04-03 19:02:42 +0530

    Initial Commit.

--- app/src/main/java/app/shopping/forevermyangle/LoginActivity.java
+++ app/src/main/java/app/shopping/forevermyangle/LoginActivity.java
@@ -1,7 +1,9 @@
 package app.shopping.forevermyangle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 public class LoginActivity extends FragmentActivity {
 
@@ -9,6 +11,12 @@ public class LoginActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+            }
+        });
         // Picasso.with(this).load(R.drawable.login_bg).into(mLayout);
     }
 }
