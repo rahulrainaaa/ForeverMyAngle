f698412b987a0c9f5c73657e515e0019ef271966
rahulrainahomeee <rahulrainahome@gmail.com>
2017-04-03 18:17:25 +0530

    Initial Commit.

--- app/src/main/java/app/shopping/forevermyangle/LoginActivity.java
+++ app/src/main/java/app/shopping/forevermyangle/LoginActivity.java
@@ -2,7 +2,6 @@ package app.shopping.forevermyangle;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.animation.AnimationUtils;
 
 public class LoginActivity extends FragmentActivity {
 
@@ -10,7 +9,6 @@ public class LoginActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        findViewById(R.id.login_layout).startAnimation(AnimationUtils.loadAnimation(this, R.anim.login_scr_appear));
+        // Picasso.with(this).load(R.drawable.login_bg).into(mLayout);
     }
 }
