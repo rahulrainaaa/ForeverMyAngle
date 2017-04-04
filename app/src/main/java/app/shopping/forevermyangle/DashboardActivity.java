22d75cfd8494e6f67c807250f5d2702901af0a03
rahulrainahomeee <rahulrainahome@gmail.com>
2017-04-04 17:02:20 +0530

    Initial Commit.

--- app/src/main/java/app/shopping/forevermyangle/DashboardActivity.java
+++ app/src/main/java/app/shopping/forevermyangle/DashboardActivity.java
@@ -3,18 +3,34 @@ package app.shopping.forevermyangle;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View view = getLayoutInflater().inflate(R.layout.app_title, null);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(view);
+
         setContentView(R.layout.activity_dashboard);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dashboard_menu, menu);
+        return true;
     }
 
     @Override
