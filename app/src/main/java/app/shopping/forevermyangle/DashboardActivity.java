3ca74741473b055ff6a332dfbbef08638387e90f
rahulrainahomeee <rahulrainahome@gmail.com>
2017-04-04 11:36:42 +0530

    Initial Commit.

--- app/src/main/java/app/shopping/forevermyangle/DashboardActivity.java
+++ app/src/main/java/app/shopping/forevermyangle/DashboardActivity.java
@@ -5,18 +5,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-    private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
     }
@@ -25,16 +21,16 @@ public class DashboardActivity extends AppCompatActivity implements BottomNaviga
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.navigation_home:
-                mTextMessage.setText(R.string.title_home);
+
                 return true;
             case R.id.navigation_dashboard:
-                mTextMessage.setText(R.string.title_dashboard);
+
                 return true;
             case R.id.navigation_notifications:
-                mTextMessage.setText(R.string.title_notifications);
+
                 return true;
             case R.id.navigation_notificationss:
-                mTextMessage.setText(R.string.title_notifications);
+
                 return true;
         }
         return false;
