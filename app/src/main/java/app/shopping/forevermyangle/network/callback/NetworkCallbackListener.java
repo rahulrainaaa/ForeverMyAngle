56c5d9d0bbdeecfdb68684c46fbaf12cd47e4d1a
rahulrainahomeee <rahulrainahome@gmail.com>
2017-04-10 17:56:39 +0530

    Initial Commit.

---
+++ app/src/main/java/app/shopping/forevermyangle/network/callback/NetworkCallbackListener.java
@@ -0,0 +1,12 @@
+package app.shopping.forevermyangle.network.callback;
+
+public interface NetworkCallbackListener {
+
+    public void networkSuccess(int requestCode);
+
+    public void networkFailure(int requestCode);
+
+    public void networkError(int requestCode);
+
+
+}
---
+++ app/src/main/java/app/shopping/forevermyangle/network/handler/NetworkHandler.java
@@ -0,0 +1,24 @@
+package app.shopping.forevermyangle.network.handler;
+
+
+import app.shopping.forevermyangle.network.callback.NetworkCallbackListener;
+
+public class NetworkHandler {
+
+    private int requestCode = -1;
+    private NetworkCallbackListener mNetworkCallbackListener = null;
+
+
+    public void httpCreate(int requestCode, NetworkCallbackListener networkCallbackListener) {
+
+    }
+
+    public void executePost(int requestCode) {
+
+    }
+
+    public void executeGet(int requestCode) {
+
+    }
+
+}
