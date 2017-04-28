e6e25cdb0843baa2559fb840cb34fe2464b0e87c
rahulrainahomeee <rahulrainahome@gmail.com>
2017-04-28 18:24:27 +0530

    Initial Commit

---
+++ app/src/main/java/app/shopping/forevermyangle/model/product/Attribute.java
@@ -0,0 +1,77 @@
+
+package app.shopping.forevermyangle.model.product;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Attribute {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("position")
+    @Expose
+    private Integer position;
+    @SerializedName("visible")
+    @Expose
+    private Boolean visible;
+    @SerializedName("variation")
+    @Expose
+    private Boolean variation;
+    @SerializedName("options")
+    @Expose
+    private List<String> options = null;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public Boolean getVariation() {
+        return variation;
+    }
+
+    public void setVariation(Boolean variation) {
+        this.variation = variation;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+}
---
+++ app/src/main/java/app/shopping/forevermyangle/model/product/Image.java
@@ -0,0 +1,87 @@
+
+package app.shopping.forevermyangle.model.product;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Image {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("date_created")
+    @Expose
+    private String dateCreated;
+    @SerializedName("date_modified")
+    @Expose
+    private String dateModified;
+    @SerializedName("src")
+    @Expose
+    private String src;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("alt")
+    @Expose
+    private String alt;
+    @SerializedName("position")
+    @Expose
+    private Integer position;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlt() {
+        return alt;
+    }
+
+    public void setAlt(String alt) {
+        this.alt = alt;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+}
