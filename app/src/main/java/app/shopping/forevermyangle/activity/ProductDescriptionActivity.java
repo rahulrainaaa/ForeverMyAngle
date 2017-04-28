d52b2e2ec7e34237267104bc2ed973e8bb1f94e7
rahulrainahomeee <rahulrainahome@gmail.com>
2017-04-28 19:03:09 +0530

    Initial Commit

---
+++ app/src/main/java/app/shopping/forevermyangle/activity/ProductDescriptionActivity.java
@@ -0,0 +1,15 @@
+package app.shopping.forevermyangle.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.shopping.forevermyangle.R;
+
+public class ProductDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_description);
+    }
+}
