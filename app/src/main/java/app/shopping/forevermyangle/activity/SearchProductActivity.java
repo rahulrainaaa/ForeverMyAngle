e4684ce776e1976420f27db2492b3002a4c79dc8
rahulrainahomeee <rahulrainahome@gmail.com>
2017-04-30 16:22:14 +0530

    Initial Commit

--- app/src/main/java/app/shopping/forevermyangle/activity/SearchProductActivity.java
+++ app/src/main/java/app/shopping/forevermyangle/activity/SearchProductActivity.java
@@ -1,7 +1,10 @@
 package app.shopping.forevermyangle.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -14,7 +17,7 @@ import app.shopping.forevermyangle.adapter.listviewadapter.ProductListViewAdapte
  * @desc Activity to show the searched products.
  */
 
-public class SearchProductActivity extends FragmentActivity {
+public class SearchProductActivity extends FragmentActivity implements AdapterView.OnItemClickListener {
 
     private GridView mProductGridList = null;
 
@@ -34,5 +37,15 @@ public class SearchProductActivity extends FragmentActivity {
         }
         ProductListViewAdapter adapter = new ProductListViewAdapter(this, R.layout.item_gridview_product, list);
         mProductGridList.setAdapter(adapter);
+        mProductGridList.setOnItemClickListener(this);
+    }
+
+    /**
+     * {@link android.widget.AdapterView.OnItemClickListener} callback methods implemented.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        startActivity(new Intent(this, ProductDescriptionActivity.class));
     }
 }
