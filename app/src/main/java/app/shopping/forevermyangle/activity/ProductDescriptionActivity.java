aec11dd46d7dcc61d6478ed9546b8f85cdaf0387
rahulrainahomeee <rahulrainahome@gmail.com>
2017-05-02 16:26:23 +0530

    Initial Commit

--- app/src/main/java/app/shopping/forevermyangle/activity/ProductDescriptionActivity.java
+++ app/src/main/java/app/shopping/forevermyangle/activity/ProductDescriptionActivity.java
@@ -1,7 +1,13 @@
 package app.shopping.forevermyangle.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import app.shopping.forevermyangle.R;
 
@@ -14,6 +20,30 @@ public class ProductDescriptionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set Custom Title.
+        View view = getLayoutInflater().inflate(R.layout.app_title, null);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(view);
+        setContentView(R.layout.activity_dashboard);
         setContentView(R.layout.activity_product_description);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.product_desc_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.option_menu_share:
+
+                Toast.makeText(this, "Share the url this product description.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
