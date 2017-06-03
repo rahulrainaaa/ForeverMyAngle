3e13b10bb6da2e56827aed7d693ff21c5e6c0571
rahulrainahomeee <rahulrainahome@gmail.com>
2017-06-03 13:03:55 +0530

    Initial Commit

--- app/src/main/java/app/shopping/forevermyangle/activity/DashboardActivity.java
+++ app/src/main/java/app/shopping/forevermyangle/activity/DashboardActivity.java
@@ -18,7 +18,7 @@ import android.widget.Toast;
 
 import app.shopping.forevermyangle.R;
 import app.shopping.forevermyangle.fragment.base.BaseFragment;
-import app.shopping.forevermyangle.fragment.fragments.CartFragment;
+import app.shopping.forevermyangle.fragment.fragments.CartDashboardFragment;
 import app.shopping.forevermyangle.fragment.fragments.CategoryDashboardFragment;
 import app.shopping.forevermyangle.fragment.fragments.ConnectionFailFragment;
 import app.shopping.forevermyangle.fragment.fragments.HomeDashboardFragment;
@@ -152,7 +152,7 @@ public class DashboardActivity extends FMAActivity implements BottomNavigationVi
                 return new CategoryDashboardFragment();
 
             case 3:     // Fragment 3 to load.
-                return new CartFragment();
+                return new CartDashboardFragment();
 
             case 4:     // Fragment 4 to load.
                 return new UserProfileFragment();
--- app/src/main/java/app/shopping/forevermyangle/adapter/listviewadapter/CartListViewAdpter.java
+++ app/src/main/java/app/shopping/forevermyangle/adapter/listviewadapter/CartListViewAdpter.java
@@ -2,8 +2,10 @@ package app.shopping.forevermyangle.adapter.listviewadapter;
 
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.shawnlin.numberpicker.NumberPicker;
+
 import java.util.ArrayList;
 
-import app.shopping.forevermyangle.activity.WishlistActivity;
+import app.shopping.forevermyangle.R;
 
 public class CartListViewAdpter extends ArrayAdapter {
 
@@ -52,7 +56,24 @@ public class CartListViewAdpter extends ArrayAdapter {
 
         View view = (View) this.inflater.inflate(resourceId, null);
 
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NumberPicker numberPicker = (NumberPicker) activity.getLayoutInflater().inflate(R.layout.layout_number_picker, null);
 
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                builder.setTitle("Quantity");
+                builder.setView(numberPicker)
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
         return view;
     }
 }
