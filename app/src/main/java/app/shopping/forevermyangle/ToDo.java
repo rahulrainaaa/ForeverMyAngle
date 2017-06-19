3ab3b349b0175696f2b7eeca0c5a107c795b2019
rahulrainahomeee <rahulrainahome@gmail.com>
2017-06-19 23:49:24 +0530

    Initial Commit

---
+++ app/src/main/java/app/shopping/forevermyangle/ToDo.java
@@ -0,0 +1,36 @@
+package app.shopping.forevermyangle;
+
+/**
+ * Created by osxuser on 6/19/17.
+ */
+
+public class ToDo {
+
+    /**
+     * Login Activity
+     *
+     * Dashboard Activity
+     * * Dashboard Fragment
+     * * Category Fragment
+     * * Cart Fragment
+     * * User Profile
+     *
+     * Show Product
+     *
+     * Wishlist Activity
+     *
+     * Rating & Review
+     *
+     * Order History List Activity
+     *
+     * Order description
+     *
+     * Terms & Conditions -- Privacy policy
+     *
+     * Register New User
+     *
+     * Forgot Password
+     *
+     *
+     */
+}
