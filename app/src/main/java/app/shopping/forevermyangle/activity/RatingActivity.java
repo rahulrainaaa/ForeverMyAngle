0c93d79d20ef34b7cd91325e5188d2fd2b9e4ffa
rahulrainahomeee <rahulrainahome@gmail.com>
2017-06-20 12:20:39 +0530

    Initial Commit

---
+++ app/src/main/java/app/shopping/forevermyangle/activity/RatingActivity.java
@@ -0,0 +1,15 @@
+package app.shopping.forevermyangle.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.shopping.forevermyangle.R;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+    }
+}
