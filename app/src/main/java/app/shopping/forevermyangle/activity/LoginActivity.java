a3051aef4b1bbb2106baf52edb38621736716c68
rahulrainahomeee <rahulrainahome@gmail.com>
2017-09-21 12:33:51 +0530

    Initial Commit

--- app/src/main/java/app/shopping/forevermyangle/activity/LoginActivity.java
+++ app/src/main/java/app/shopping/forevermyangle/activity/LoginActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
